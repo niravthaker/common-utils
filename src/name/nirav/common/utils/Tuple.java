dc30083c14fbb6d9a1603d63b995b1db06b4564b
Nirav Thaker <nirav.thaker@gmail.com>
2010-12-08 21:15:08 -0500

    Optionified

--- src/name/nirav/common/utils/Tuple.java
+++ src/name/nirav/common/utils/Tuple.java
@@ -1,5 +1,11 @@
 package name.nirav.common.utils;
 
+import java.util.Arrays;
+import java.util.List;
+
+import name.nirav.common.utils.monads.Option;
+import name.nirav.common.utils.monads.Options;
+
 public class Tuple<A, B> {
 	private final A first;
 	private final B rest;
@@ -16,8 +22,15 @@ public class Tuple<A, B> {
 		return rest;
 	}
 	
+	public List<Object> untypedValues() {
+	    return Arrays.asList(first, rest);
+	}
+	
 	public static <A, B> Tuple<A, B> create(A first, B rest){
 		return new Tuple<A, B>(first, rest);
 	}
+    public static <A, B> Tuple<Option<A>, Option<B>> createOptional(A first, B rest){
+        return new Tuple<Option<A>, Option<B>>(Options.wrap(first), Options.wrap(rest));
+    }
 
 }
